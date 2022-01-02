125fb060370e2169f12244063010ea2e1fc10517
Pedro PP <1qwertyuiop.wasd@gmail.com>
2022-01-02 21:54:15 +0000

    Better regex for syntax highlighting - fixed normal words between 2 strings being treated as part of the string

--- src/main/editor/SyntaxHighlighting.java
+++ src/main/editor/SyntaxHighlighting.java
@@ -13,7 +13,7 @@ public class SyntaxHighlighting
     private final TextEditor editor;
     private final JTextPane textPane;
 
-    private final String keywords = "if else while do include memory proc const end offset reset assert in print dup over drop swap";
+    private final String keywords = "if else elif while do include memory proc const end offset reset assert in print dup over drop swap";
 
     private final StyledDocument doc;
 
@@ -50,7 +50,9 @@ public class SyntaxHighlighting
 
         String txt = textPane.getText(startT, (endT-1)-startT);
 
-        Pattern p = Pattern.compile("[a-zA-Z]+|//.+|\".+\"");
+        //\/\/.+|\"[a-zA-Z ]+\"+|[a-z0-9A-Z_+\-><\(\)]+
+        //comment | string | anything else
+        Pattern p = Pattern.compile("//.+|\"[0-9a-zA-Z _\\-?]+\"+|[a-z0-9A-Z_+\\-><()]+");
         Matcher match = p.matcher(txt);
 
         //System.out.println("Working line "+t);
@@ -73,93 +75,87 @@ public class SyntaxHighlighting
 
     private void StyleAll(ArrayList<EditorWord> wrds)
     {
-        Runnable doHighlightAll = new Runnable() {
-            @Override
-            public void run()
+        Runnable doHighlightAll = () ->
+        {
+            for (EditorWord a :wrds)
             {
-                for (EditorWord a :wrds)
+                String wrd = a.getWord();
+                try
                 {
-                    String wrd = a.getWord();
-                    try
-                    {
-                        wrd = textPane.getText(a.getStartIndex(), a.getEndIndex() - a.getStartIndex());
-                        wrd = wrd.trim();
-                        wrd = wrd.replace('\n', '\0');
-                        //System.out.println("Word is "+wrd);
-                    } catch (BadLocationException e)
-                    {
-                        System.out.println("Bad location at " + a.getStartIndex() + " - " + a.getEndIndex());
-                    }
+                    wrd = textPane.getText(a.getStartIndex(), a.getEndIndex() - a.getStartIndex());
+                    wrd = wrd.trim();
+                    wrd = wrd.replace('\n', '\0');
+                    //System.out.println("Word is "+wrd);
+                } catch (BadLocationException e)
+                {
+                    System.out.println("Bad location at " + a.getStartIndex() + " - " + a.getEndIndex());
+                }
 
-                    if (wrd == null || wrd.length() == 0) return;
+                if (wrd == null || wrd.length() == 0) return;
 
-                    if (wrd.startsWith("\""))
+                if (wrd.startsWith("\""))
+                {
+                    doc.setCharacterAttributes(a.getStartIndex(), wrd.length(), doc.getStyle("string"), true);
+                } else
+                {
+                    if (wrd.startsWith("//"))
                     {
-                        doc.setCharacterAttributes(a.getStartIndex(), wrd.length(), doc.getStyle("string"), true);
+                        doc.setCharacterAttributes(a.getStartIndex(), wrd.length(), doc.getStyle("comment"), true);
+                    } else if (Arrays.asList(keywords.split(" ")).contains(wrd))
+                    {
+                        doc.setCharacterAttributes(a.getStartIndex(), wrd.length(), doc.getStyle("keyword"), true);
                     } else
                     {
-                        if (wrd.startsWith("//"))
-                        {
-                            doc.setCharacterAttributes(a.getStartIndex(), wrd.length(), doc.getStyle("comment"), true);
-                        } else if (Arrays.asList(keywords.split(" ")).contains(wrd))
-                        {
-                            doc.setCharacterAttributes(a.getStartIndex(), wrd.length(), doc.getStyle("keyword"), true);
-                        } else
-                        {
-                            doc.setCharacterAttributes(a.getStartIndex(), wrd.length(), doc.getStyle("regular"), true);
-                        }
+                        doc.setCharacterAttributes(a.getStartIndex(), wrd.length(), doc.getStyle("regular"), true);
                     }
-
-                    a.setPainted(true);
-                    doc.setLogicalStyle(textPane.getText().length() - 1, doc.getStyle("regular"));
                 }
+
+                a.setPainted(true);
+                doc.setLogicalStyle(textPane.getText().length() - 1, doc.getStyle("regular"));
             }
         };
         SwingUtilities.invokeLater(doHighlightAll);
     }
 
-    private void StyleWord(int start, int end) throws BadLocationException
+    private void StyleWord(int start, int end)
     {
-        Runnable doHighlight = new Runnable() {
-            @Override
-            public void run()
+        Runnable doHighlight = () ->
+        {
+            String wrd = null;
+            try
             {
-                String wrd = null;
-                try
+                wrd = textPane.getText(start, end-start);
+                wrd = wrd.trim();
+                wrd = wrd.replace('\n','\0');
+                //System.out.println("Word is "+wrd);
+            } catch (BadLocationException e)
+            {
+                System.out.println("Bad location at "+start+" - "+end);
+            }
+
+            if (wrd==null||wrd.length()==0)return;
+
+            if (wrd.startsWith("\""))
+            {
+                doc.setCharacterAttributes(start, wrd.length(), doc.getStyle("string"), true);
+            }
+            else
+            {
+                if (wrd.startsWith("//"))
                 {
-                    wrd = textPane.getText(start, end-start);
-                    wrd = wrd.trim();
-                    wrd = wrd.replace('\n','\0');
-                    //System.out.println("Word is "+wrd);
-                } catch (BadLocationException e)
-                {
-                    System.out.println("Bad location at "+start+" - "+end);
+                    doc.setCharacterAttributes(start, wrd.length(), doc.getStyle("comment"), true);
                 }
-
-                if (wrd==null||wrd.length()==0)return;
-
-                if (wrd.startsWith("\""))
+                else if (Arrays.asList(keywords.split(" ")).contains(wrd))
                 {
-                    doc.setCharacterAttributes(start, wrd.length(), doc.getStyle("string"), true);
+                    doc.setCharacterAttributes(start, wrd.length(), doc.getStyle("keyword"), true);
                 }
                 else
                 {
-                    if (wrd.startsWith("//"))
-                    {
-                        doc.setCharacterAttributes(start, wrd.length(), doc.getStyle("comment"), true);
-                    }
-                    else if (Arrays.asList(keywords.split(" ")).contains(wrd))
-                    {
-                        doc.setCharacterAttributes(start, wrd.length(), doc.getStyle("keyword"), true);
-                    }
-                    else
-                    {
-                        doc.setCharacterAttributes(start, wrd.length(), doc.getStyle("regular"), true);
-                    }
+                    doc.setCharacterAttributes(start, wrd.length(), doc.getStyle("regular"), true);
                 }
-
-                doc.setLogicalStyle(textPane.getText().length()-1,doc.getStyle("regular"));
             }
+
+            doc.setLogicalStyle(textPane.getText().length()-1,doc.getStyle("regular"));
         };
         SwingUtilities.invokeLater(doHighlight);
     }
